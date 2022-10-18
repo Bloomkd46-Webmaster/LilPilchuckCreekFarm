9617a287621958f7b7f9e6870dead6b9c770066c
bloomkd46 <bloomkd46@icloud.com>
2022-10-18 07:32:01 -0700

    started to add specific buck/doe page

--- src/app/does/senior-does/senior-does.component.ts
+++ src/app/does/senior-does/senior-does.component.ts
@@ -1,7 +1,10 @@
 import { ColorSchemeService } from 'src/app/color-scheme.service';
 import { Goat } from 'src/app/goat.interface';
+import { MetaService } from 'src/app/meta.service';
 
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
 
 import does from './senior-does.json';
 
@@ -12,9 +15,26 @@ import does from './senior-does.json';
   templateUrl: './senior-does.component.html',
   styleUrls: ['./senior-does.component.scss']
 })
-export class SeniorDoesComponent {
+export class SeniorDoesComponent implements OnInit, OnDestroy {
   public does: Goat[] = does.goats;
+  public doe?: Goat;
 
-  constructor(public colorScheme: ColorSchemeService) { }
-
+  constructor(public colorScheme: ColorSchemeService, private activatedRoute: ActivatedRoute, private metaService: MetaService, private meta: Meta) { }
+  ngOnInit(): void {
+    const specificDoe: boolean = this.activatedRoute.snapshot.paramMap.get("doe") !== null;
+    if (specificDoe) {
+      this.doe = this.does.find(doe => doe.nickname === this.activatedRoute.snapshot.paramMap.get("doe"));
+      if (this.doe) {
+        console.log(this.doe);
+        //this.metaService.updateKeywords(['News', 'Post', 'Blog', ...(this.post.categories ?? [])]);
+        this.metaService.updateTitle(this.doe.nickname);
+        this.doe.description !== undefined ? this.metaService.updateDescription(this.doe.description) : undefined;
+      } else {
+        this.meta.addTag({ name: 'robots', content: 'NOINDEX' });
+      }
+    }
+  }
+  ngOnDestroy(): void {
+    this.meta.removeTag('name="robots"');
+  }
 }
