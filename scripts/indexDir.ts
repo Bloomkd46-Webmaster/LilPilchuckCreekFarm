f3b35bdabaf1b4bdb77a2c68ac2fcf1bbf29f324
Kolton Bloom <bloomkd46@icloud.com>
2022-11-13 19:58:17 -0800

    moved goat images into goat dir

--- scripts/indexDir.ts
+++ scripts/indexDir.ts
@@ -5,5 +5,5 @@ import * as path from 'path';
 
 
 //@ts-ignore
-const tree = dirTree(path.join(__dirname, "../src/assets"), { exclude: /map.json/ });
-fs.writeFileSync(path.join(__dirname, "../src/assets/map.json"), JSON.stringify(tree, null, 2).split(path.join(__dirname, '../src').split('\\').join('\\\\')).join('').split('\\\\').join('/'));
+const tree = dirTree(path.join(__dirname, "../src/assets/goats"), { exclude: /map.json/ });
+fs.writeFileSync(path.join(__dirname, "../src/assets/goats/map.json"), JSON.stringify(tree, null, 2).split(path.join(__dirname, '../src').split('\\').join('\\\\')).join('').split('\\\\').join('/'));
