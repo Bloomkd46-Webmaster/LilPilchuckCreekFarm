35a8ba1714e707b982f80b553a92d9ec107878a8
Kolton Bloom <bloomkd46@icloud.com>
2022-11-25 11:47:12 -0800

    better image loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule, Meta } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BucksComponent } from './bucks/bucks.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 import { DoesComponent } from './does/does.component';
 import { GoatModalComponent } from './goat-modal/goat-modal.component';
 import { GoatComponent } from './goat/goat.component';
 import { HomeComponent } from './home/home.component';
 import { KiddingScheduleComponent } from './kidding-schedule/kidding-schedule.component';
-import { ContactUsComponent } from './contact-us/contact-us.component';
 
 
 
@@ -26,7 +27,8 @@ import { ContactUsComponent } from './contact-us/contact-us.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [Meta],
   bootstrap: [AppComponent]
--- src/app/image.service.ts
+++ src/app/image.service.ts
@@ -1,5 +1,6 @@
 
 
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import images from '../assets/goats/map.json';
@@ -11,10 +12,26 @@ import { Goat } from './goat.interface';
   providedIn: 'root'
 })
 export class ImageService {
+  images = images;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    console.log('Fetching pre-compiled images...');
+    this.http.get('assets/images/map.data.json').subscribe((data: any) => {
+      console.log('Fetched pre-compiled images');
+      this.images = data;
+    });
+  }
 
   find(goat: Goat) {
-    return images.children.find(child => child.name === goat.nickname)?.children;//?.map(image => image.path);
+    return this.images.children.find(child => child.name === goat.nickname)?.children;//?.map(image => image.path);
+  }
+
+  extractDisplay(images: Images): Images[number] {
+    return images.find(image => image.name.startsWith('display')) ?? images[0];
   }
 }
+export type Images = ({
+  path: string;
+  name: string;
+  children?: Images;
+})[];
