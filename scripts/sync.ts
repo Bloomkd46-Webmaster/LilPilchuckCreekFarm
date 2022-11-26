70887c4f654a7c604f9b38947d09f478e8e87a40
Kolton Bloom <bloomkd46@icloud.com>
2022-11-26 12:33:25 -0800

    improved sync script

--- scripts/sync.ts
+++ scripts/sync.ts
@@ -1,4 +1,4 @@
-import ADGA, { OwnedGoats } from 'adga';
+import ADGA, { Awards, Goats, OwnedGoats } from 'adga';
 import * as fs from 'fs';
 import { stdin as input, stdout as output } from 'node:process';
 import * as readline from 'node:readline/promises';
@@ -7,7 +7,7 @@ import * as path from 'path';
 
 
 const rl = readline.createInterface({ input, output });
-const config: { does: OwnedGoats['result']['items'] & { nickname: string; description: string; }[]; bucks: OwnedGoats['result']['items'] & { nickname: string; description: string; }[]; } = require('../src/app/goats.json');
+const config: { does: (OwnedGoats['result']['items'][number] & { nickname: string; description: string; awards: Awards['result']['items']; })[]; bucks: (OwnedGoats['result']['items'][number] & { nickname: string; description: string; })[]; awards: Awards['result']['items']; } = require('../src/app/goats.json');
 const credentials: { username?: string, password?: string, accountId?: number; } = {};
 /** Remove the first two irrelevant arguments */
 const myArgs = process.argv.slice(2);
@@ -33,25 +33,70 @@ function titleCase(string: string) {
   return sentence.join(' ');
 }
 (async () => {
+  console.log('Initializing...');
   const adga = await ADGA.init(credentials.username!, credentials.password!);
+  console.log('Downloading Goats...');
   const goats = await adga.getOwnedGoats(credentials.accountId || undefined);
+  console.log('Downloaded', goats.totalCount, 'Goats From ADGA');
   for (const goat of goats.items) {
+    const awards = (await adga.getAwards(goat.id)).items;
     const doe = config.does.find(configGoat => configGoat.name === goat.name);
     const buck = config.bucks.find(configGoat => configGoat.name === goat.name);
     if (doe) {
-      //@ts-ignore
-      Object.assign(config.does[config.does.indexOf(doe)], goat, { description: config.does[config.does.indexOf(doe)].description || await rl.question(`What would you like to set the description to for '${(doe).nickname}'?\n(optional) `) || '', });
+      console.log(`Updating ${doe.nickname}...`);
+      Object.assign(config.does[config.does.indexOf(doe)], goat, { description: config.does[config.does.indexOf(doe)].description || await rl.question(`What would you like to set the description to for '${doe.nickname}'?\n(optional) `) || '', awards: awards });
     } else if (buck) {
-      //@ts-ignore
-      Object.assign(config.bucks[config.bucks.indexOf(buck)], goat, { description: config.bucks[config.bucks.indexOf(buck)].description || await rl.question(`What would you like to set the description to for '${(buck).nickname}'?\n(optional) `) || '', });
+      console.log(`Updating ${buck.nickname}...`);
+      Object.assign(config.bucks[config.bucks.indexOf(buck)], goat, { description: config.bucks[config.bucks.indexOf(buck)].description || await rl.question(`What would you like to set the description to for '${buck.nickname}'?\n(optional) `) || '', awards: awards });
     } else {
+      console.log('Creating', goat.name);
       const nickname = titleCase(await rl.question(`What would you like to set the nickname to for '${goat.name}'?\n(${titleCase(goat.name.split(' ').pop()!)}) `) || goat.name.split(' ').pop()!);
       config[goat.sex === 'Female' ? 'does' : 'bucks'].push(Object.assign(goat, {
         nickname: nickname,
         description: await rl.question(`What would you like to set the description to for '${nickname}'?\n(optional) `) || '',
+        awards: awards
       }));
     }
   }
+  console.log('Saving Updates...');
   fs.writeFileSync(path.join(__dirname, '../src/app/goats.json'), JSON.stringify(config, null, 2));
+  console.log('Downloading Reference Goats...');
+  const newConfig: { does: OwnedGoats['result']['items'] & { nickname: string; description: string; }[]; bucks: OwnedGoats['result']['items'] & { nickname: string; description: string; }[]; references: (Goats['result']['items'][number] & { awards: Awards['result']['items']; })[]; } = require('../src/app/goats.json');
+  const ids = newConfig.references.map(reference => reference.id);
+  for (const goat of [...newConfig.does, ...newConfig.bucks]) {
+    const dam = newConfig.references.find(reference => reference.id === goat.damId);
+    const sire = newConfig.references.find(reference => reference.id === goat.sireId);
+    if (!dam && !ids.includes(goat.damId)) {
+      ids.push(goat.damId);
+    }
+    if (!sire && !ids.includes(goat.sireId)) {
+      ids.push(goat.sireId);
+    }
+  }
+  let referenceGoats = (await adga.getGoats(ids)).items;
+  for (const referenceGoat of referenceGoats) {
+    if (!ids.includes(referenceGoat.damId)) {
+      ids.push(referenceGoat.damId);
+    }
+    if (!ids.includes(referenceGoat.sireId)) {
+      ids.push(referenceGoat.sireId);
+    }
+  }
+  referenceGoats = (await adga.getGoats(ids)).items;
+  console.log('Downloaded', referenceGoats.length, 'Reference Goats From ADGA.');
+  for (const goat of referenceGoats) {
+    const awards = (await adga.getAwards(goat.id)).items;
+    const reference = newConfig.references.find(reference => reference.id === goat.id);
+    if (reference) {
+      console.log(`Updating ${reference.name}...`);
+      Object.assign(newConfig.references[newConfig.references.indexOf(reference)], goat, { awards: awards });
+    } else {
+      console.log(`Creating ${goat.name}...`);
+      newConfig.references.push(Object.assign(goat, { awards: awards }));
+    }
+  }
+  console.log('Saving Reference Goats...');
+  fs.writeFileSync(path.join(__dirname, '../src/app/goats.json'), JSON.stringify(newConfig, null, 2));
+  console.log('Done.');
   process.exit();
 })();
