31928ce37f1cb5693b8211ace960ab65a35be6c4
Kolton Bloom <bloomkd46@icloud.com>
2023-04-19 07:10:48 -0700

    cleanup

--- scripts/indexDir.ts
+++ scripts/indexDir.ts
@@ -5,8 +5,8 @@ import * as path from 'path';
 
 //@ts-ignore
 const tree: { children: Images; } = dirTree(path.join(__dirname, "../src/assets/goats"), { exclude: /\.json$|.DS_Store/ });
-console.debug(tree);
-const generateLink = async (child: any) => {
+//console.debug(tree);
+/*const generateLink = async (child: any) => {
   if (child.children) {
     for (const child1 of child.children) {
       child.children[child.children.indexOf(child1)] = await generateLink(child1);
@@ -16,7 +16,7 @@ const generateLink = async (child: any) => {
     child.path = `data:image/${path.extname(child.name).replace('.', '')};base64,${fs.readFileSync(child.path, 'base64')}`;
   }
   return child;
-};
+};*/
 console.log('Indexing');
 fs.writeFileSync(path.join(__dirname, "../src/assets/goats/map.json"), JSON.stringify(tree, null, 2).split(path.join(__dirname, '../src').split('\\').join('\\\\')).join('').split('\\\\').join('/'));
 ////generateLink(tree).then(children => fs.writeFileSync(path.join(__dirname, "../src/assets/goats/map.data.json"), JSON.stringify(children, null, 2).split(path.join(__dirname, '../src').split('\\').join('\\\\')).join('').split('\\\\').join('/')));
