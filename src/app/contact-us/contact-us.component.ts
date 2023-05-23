e2aa779d273f6a43bf2a129d61ba4dd8b1cb9ac5
Kolton Bloom <bloomkd46@icloud.com>
2023-05-23 12:15:36 -0700

    fixed lint errors

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
   styleUrls: ['./contact-us.component.scss']
 })
-export class ContactUsComponent implements OnInit {
+export class ContactUsComponent /*implements OnInit*/ {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  /*ngOnInit(): void {
+  }*/
 
 }
