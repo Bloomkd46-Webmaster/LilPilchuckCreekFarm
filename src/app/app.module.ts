fbfb218f7104b4417af75580092e5ba8474ebe29
Kolton Bloom <bloomkd46@icloud.com>
2023-06-23 15:11:02 -0700

    fixed broken app module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,21 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule, Meta } from '@angular/platform-browser';
 
+import { AdgaInfoComponent } from './adga-info/adga-info.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BucksComponent } from './bucks/bucks.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { DoesComponent } from './does/does.component';
+import { ForSaleGoatModalComponent } from './for-sale-goat-modal/for-sale-goat-modal.component';
+import { ForSaleGoatComponent } from './for-sale-goat/for-sale-goat.component';
+import { ForSaleComponent } from './for-sale/for-sale.component';
 import { GoatModalComponent } from './goat-modal/goat-modal.component';
 import { GoatComponent } from './goat/goat.component';
 import { HomeComponent } from './home/home.component';
 import { KiddingScheduleComponent } from './kidding-schedule/kidding-schedule.component';
-import { PetsComponent } from './pets/pets.component';
-import { ForSaleComponent } from './for-sale/for-sale.component';
-import { ForSaleGoatComponent } from './for-sale-goat/for-sale-goat.component';
-import { ForSaleGoatModalComponent } from './for-sale-goat-modal/for-sale-goat-modal.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { PetsComponent } from './pets/pets.component';
 
 
 @NgModule({
@@ -34,7 +35,8 @@ import { NotFoundComponent } from './not-found/not-found.component';
     ForSaleComponent,
     ForSaleGoatComponent,
     ForSaleGoatModalComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    AdgaInfoComponent
   ],
   imports: [
     BrowserModule,
