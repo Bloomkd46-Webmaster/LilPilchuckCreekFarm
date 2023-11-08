54816501176c76136f33088af3e3ecbe9f86458d
Kolton Bloom <bloomkd46@icloud.com>
2023-11-08 11:12:12 -0800

    (hopefully) fixed microsoft clarity script not being appended properly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -52,13 +52,11 @@ export class AppComponent implements OnInit {
     if (app.clarity) {
       const script = document.createElement('script');
       script.innerHTML =
-        `<script type="text/javascript">
-    (function(c,l,a,r,i,t,y){
+        `(function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "${app.clarity}");
-</script>`;
+    })(window, document, "clarity", "script", "${app.clarity}");`;
       document.head.appendChild(script);
     }
 
